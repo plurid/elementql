cc9cd5db1bd5fd1fa5a9fe80e9b499d32baba0f3
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-07 23:20:51 +0300

    elementql-parser > setup: basic

---
+++ packages/elementql-parser/src/index.ts
@@ -0,0 +1,5 @@
+import ElementQLParser from './objects/ElementQLParser';
+
+
+
+export default ElementQLParser;
---
+++ packages/elementql-parser/src/objects/ElementQLParser/__specs__/index.spec.ts
@@ -0,0 +1,9 @@
+import ElementQLParser from '../';
+
+
+
+describe('ElementQLParser basic', () => {
+    it('works', () => {
+        expect(true).toBe(true);
+    });
+});
