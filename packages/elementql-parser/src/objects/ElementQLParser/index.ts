49df32ef570af2570efc67776a7158d49cea9a54
Jan Cavel <caveljan@gmail.com>
2019-09-07 23:20:51 +0300

    elementql-parser > setup: basic

---
+++ packages/elementql-parser/src/objects/ElementQLParser/index.ts
@@ -0,0 +1,13 @@
+import {
+    IElementQLParser,
+} from '../../interfaces';
+
+
+
+class ElementQLParser implements IElementQLParser {
+    constructor(options?: any) {
+    }
+}
+
+
+export default ElementQLParser;
