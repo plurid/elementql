6f86b93ec457ec9e602751aa5e8afc1c9133d14f
Jan Cavel <caveljan@gmail.com>
2019-09-07 11:42:37 +0300

    elementql > setup: basic

---
+++ packages/elementql/src/interfaces/index.ts
@@ -0,0 +1,8 @@
+export interface IElementQL {
+
+}
+
+
+export interface ElementQLOptions {
+
+}
---
+++ packages/elementql/src/objects/ElementQL/index.ts
@@ -0,0 +1,15 @@
+import {
+    IElementQL,
+    ElementQLOptions,
+} from '../../interfaces';
+
+
+
+class ElementQL implements IElementQL {
+    constructor(options?: ElementQLOptions) {
+
+    }
+}
+
+
+export default ElementQL;
