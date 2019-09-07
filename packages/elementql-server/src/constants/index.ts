9ee27388267fc8bb081f114de3e83db5104ae51f
Jan Cavel <caveljan@gmail.com>
2019-09-07 16:08:40 +0300

    elementql-server > dev: ElementQLServer

--- packages/elementql-server/src/constants/index.ts
+++ packages/elementql-server/src/constants/index.ts
@@ -1,6 +1,10 @@
 import path from 'path';
 
 
-export const DEFAULT_PORT = 33500;
+export const DEFAULT_PORT = 33300;
+
+export const DEFAULT_ELEMENTS_DIR = '/elements';
+
+export const DEFAULT_ENDPOINT = '/elementql';
 
 export const FAVICON = path.join(__dirname, '../src/assets', 'favicon.ico');
