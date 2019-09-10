7e7b2732df53268c8d745f594a91a8bec93fac06
Jan Cavel <caveljan@gmail.com>
2019-09-10 09:31:44 +0300

    elementql-parser > dev: query parsing

--- packages/elementql-parser/src/objects/ElementQLParser/__specs__/index.spec.ts
+++ packages/elementql-parser/src/objects/ElementQLParser/__specs__/index.spec.ts
@@ -45,4 +45,97 @@ import {
 
         expect(result).toStrictEqual(elements);
     });
+
+    it('gets multiple elements', () => {
+        const query = `
+export {
+    element <ElementFoo>
+    element <Element-Foo>
+    element <Element-Foo-Bar3>
+    element <element-1>
+    element <element-2>
+
+    space page {
+        element <element-page>
+    }
+}
+        `;
+        const elements = [ {name: 'element'} ];
+        const parser = new ElementQLParser(query);
+        const result = parser.parse();
+
+        expect(result).toStrictEqual(elements);
+    });
+
+    it('gets multiple spaces', () => {
+        const query = `
+export {
+    element <element-1>
+    element <element-2>
+
+    space page {
+        element <element-page>
+    }
+
+    space subpage {
+        element <element-subpage>
+    }
+}
+        `;
+        const elements = [ {name: 'element'} ];
+        const parser = new ElementQLParser(query);
+        const result = parser.parse();
+
+        expect(result).toStrictEqual(elements);
+    });
+
+    it.only('gets multiple spaces and subspaces', () => {
+        const query = `
+export {
+    element <element-1>
+    element <element-2> {
+        self
+        element <element-2-1>
+    }
+
+    space page {
+        element <element-page> {
+            element <element-page-1>
+        }
+
+        space subspace {
+            element <element-subspace>
+        }
+    }
+
+    space subpage {
+        element <element-subpage>
+    }
+}
+        `;
+
+        const expectedResult = {
+            element1: 'element-1',
+            element2: {
+                self: 'element-2',
+                element21: 'element-2-1',
+            },
+            page: {
+                elementPage: {
+                    elementPage1: 'element-page-1',
+                },
+                subspace: {
+                    elementSubspace: 'element-subspace',
+                },
+            },
+            subPage: {
+                elementSubpage: 'element-subpage',
+            },
+        };
+
+        const parser = new ElementQLParser(query);
+        const result = parser.parse();
+
+        expect(result).toStrictEqual(expectedResult);
+    });
 });
--- packages/elementql-parser/src/objects/ElementQLParser/index.ts
+++ packages/elementql-parser/src/objects/ElementQLParser/index.ts
@@ -13,34 +13,93 @@ class ElementQLParser implements IElementQLParser {
     }
 
     public parse(): ElementQL[] {
-        let query = this.query
-        query = query.replace(/\\n/g, ' ');
-        query = query.replace(/\s+/g, ' ');
-        query = query.trim();
+        console.log('this.query', this.query);
+        let query = this.query.split('\n');
+        query = query.map(el => el.trim());
+        // console.log('query', query);
+        query = query.filter(el => el !== '');
+        // console.log(query);
+        // check if export or import
+        let exporting = /export/.test(query[0]);
+        let importing = /import/.test(query[0]);
+        query = query.slice(1, query.length - 1);
 
-        const elements: ElementQL[] = [];
-        const importRE = new RegExp('^import.*{((.|\\n)*)}$', 'm');
-        const importMatch = query.match(importRE);
-        // console.log('importMatch', importMatch);
+        console.log(exporting, importing);
+        console.log(query);
 
-        if (importMatch) {
-            const elementsString = importMatch[1];
-            const elementsRE = new RegExp('<(.*)>', 'g');
-            const elementsMatch = elementsString.trim().match(elementsRE);
-            // console.log('elementsMatch', elementsMatch);
+        // from the query extract
+        // the root elements (with their subelements)
+        // the root spaces (with their subspaces and elements)
 
-            if (elementsMatch) {
-                for (let elementMatch of elementsMatch) {
-                    const name = elementMatch.replace('<', '').replace('>', '');
-                    const element: ElementQL = {
-                        name,
-                    };
-                    elements.push(element);
-                }
-            }
-        }
 
-        return elements;
+        // let query = this.query;
+        // // query = query.replace(/\\n/g, ' ');
+        // query = query.replace(/\s+/g, ' ');
+        // query = query.trim();
+
+        // const importRE = new RegExp('^import.*{((.|\\n)*)}$', 'm');
+        // const importMatch = query.match(importRE);
+        // // console.log('importMatch', importMatch);
+        // if (importMatch) {
+        //     const elements: ElementQL[] = [];
+
+        //     const elementsString = importMatch[1];
+        //     const elementsRE = new RegExp('<(.*)>', 'g');
+        //     const elementsMatch = elementsString.trim().match(elementsRE);
+        //     // console.log('elementsMatch', elementsMatch);
+
+        //     if (elementsMatch) {
+        //         for (let elementMatch of elementsMatch) {
+        //             const name = elementMatch.replace('<', '').replace('>', '');
+        //             const element: ElementQL = {
+        //                 name,
+        //             };
+        //             elements.push(element);
+        //         }
+        //     }
+
+        //     return elements;
+        // }
+
+
+        // const exportRE = new RegExp('^export\\s{\\s(.+)\\s}$', 'm');
+        // const exportMatch = query.match(exportRE);
+        // console.log('exportMatch', exportMatch);
+        // console.log(query);
+        // if (exportMatch) {
+        //     const elements: ElementQL[] = [];
+        //     const elementsString = exportMatch[1].trim();
+
+        //     const spacesRE = new RegExp('space\\s\\w+\\s{\\s.*?\\s}', 'g');
+        //     const spacesMatch = elementsString.match(spacesRE);
+        //     console.log('spacesMatch', spacesMatch);
+
+        //     // to parse the exportMatch[1] for spaces
+        //     // store the spaces in a data structure
+        //     // check for elements within the spaces which are leaves and which are trees
+        //     // remove the extracted data from elementsString
+        //     // search for root elements (elements outside spaces)
+
+        //     const elementsRE = new RegExp('(element\\s<(\\w+-?(\\w+|\\d+?))*>)', 'g');
+        //     const elementsMatch = elementsString.trim().match(elementsRE);
+        //     console.log('elementsMatch', elementsMatch);
+
+        //     if (elementsMatch) {
+        //         for (let elementMatch of elementsMatch) {
+        //             const name = elementMatch.replace('element <', '').replace('>', '');
+        //             const element: ElementQL = {
+        //                 name,
+        //             };
+        //             elements.push(element);
+        //         }
+        //     }
+
+        //     console.log(elements);
+
+        //     return elements;
+        // }
+
+        return [];
     }
 }
 
