ededc9e15daeb18e619818aa0fb77cf4f2a41047
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-10 16:58:12 +0300

    elementql-client > dev: ElementQLClient

--- packages/elementql-client/src/interfaces/index.ts
+++ packages/elementql-client/src/interfaces/index.ts
@@ -6,3 +6,15 @@ export interface ElementQLClient {
 export interface ElementQLClientOptions {
     url: string;
 }
+
+
+export interface ElementQLElements {
+    [key: string]: any;
+}
+
+
+declare global {
+    interface Window {
+        elementql: ElementQLElements;
+    }
+}
