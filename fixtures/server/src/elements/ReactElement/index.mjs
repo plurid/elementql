0983cc15ee4972fe65bdc5149087f468aa95aa55
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-09 19:55:51 +0300

    fixtures/server > dev: elements

--- fixtures/server/src/elements/ReactElement/index.mjs
+++ fixtures/server/src/elements/ReactElement/index.mjs
@@ -1,3 +1,8 @@
+// to have the imports of utiltiies/libraries from the elementql endpoint
+// import React from 'https://api.domain.com/elementql/node_modules/react';
+
+
+
 const AnElement = () => {
     return React.createElement('div', null, `Hello from element`);
 };
