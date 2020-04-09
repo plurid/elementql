427a7b79e33836d56cf7a930562e71fcf1a206a4
Jan Cavel <caveljan@gmail.com>
2020-04-09 16:07:26 +0300

    setup: package

--- packages/rollup.plugins.js
+++ packages/rollup.plugins.js
@@ -1,22 +1,20 @@
 import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import depsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
-import resolve from 'rollup-plugin-node-resolve';
-import url from 'rollup-plugin-url';
-import replace from '@rollup/plugin-replace';
+// import postcss from 'rollup-plugin-postcss';
+import resolve from '@rollup/plugin-node-resolve';
+// import replace from '@rollup/plugin-replace';
 
 
 
 const plugins = [
-    replace({
-        'process.env.MODE_ENV': JSON.stringify(process.env.MODE_ENV),
-    }),
+    // replace({
+    //     'process.env.MODE_ENV': JSON.stringify(process.env.MODE_ENV),
+    // }),
     depsExternal(),
-    postcss({
-        modules: true,
-    }),
-    url(),
+    // postcss({
+    //     modules: true,
+    // }),
     resolve({
         modulesOnly: true,
     }),
