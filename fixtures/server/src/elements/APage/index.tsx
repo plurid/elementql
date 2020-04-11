22b5e2e574ae9f22eb49b89308dac55094866629
Jan Cavel <caveljan@gmail.com>
2020-04-11 08:37:32 +0300

    fixtures/server > dev: elements

---
+++ fixtures/server/src/elements/APage/index.tsx
@@ -0,0 +1,19 @@
+// @ts-nocheck
+import React from 'react';
+
+import AHeader from './AHeader';
+import AFooter from './AFooter';
+
+
+const Page: React.FC<any> = () => {
+    return (
+        <div>
+            <AHeader />
+
+            <AFooter />
+        </div>
+    );
+}
+
+
+export default Page;
