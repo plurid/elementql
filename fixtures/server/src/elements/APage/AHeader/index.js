83b8bf2d391bc8994a132b0fc326865aad29b15a
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-11 08:54:54 +0300

    fixtures/server > dev: elements

---
+++ fixtures/server/src/elements/APage/AHeader/index.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+import React from 'react';
+
+
+
+var Header = function () {
+    return (
+        React.createElement("div", null, "Header")
+    );
+};
+
+
+export default Header;
