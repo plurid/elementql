b53fb3b45591185b92de8d820b20613d5882bf28
Jan Cavel <caveljan@gmail.com>
2020-04-12 11:07:54 +0300

    elementql-server-node > dev: ElementQLServer | html

---
+++ packages/elementql-server-node/source/utilities/html.ts
@@ -0,0 +1,30 @@
+export const indexHTML = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ElementQL</title>
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+            background: #1C1E22;
+            color: white;
+            display: grid;
+            place-content: center;
+            font-family: Ubuntu, sans-serif;
+            font-size: 20px;
+        }
+        a {
+            color: #ccc;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <a href="https://elementql.org" target="_blank" rel="noopener noreferrer">
+        ElementQL
+    </a>
+</body>
+</html>
+`;
