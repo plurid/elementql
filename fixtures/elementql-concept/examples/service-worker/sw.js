398a299a2f6b93ac88043b0642101e91246d35b1
Jan Cavel <caveljan@gmail.com>
2020-04-12 22:29:53 +0300

    fixtures/elementql-concept > dev: examples | service-worker

--- fixtures/elementql-concept/examples/service-worker/sw.js
+++ fixtures/elementql-concept/examples/service-worker/sw.js
@@ -1,30 +1,61 @@
 const removeSpaces = str => str.split(/^ +/m).join('').trim();
 
+const globalMap = {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
+};
+
+const getGlobalByUrl = (url) => Object.keys(globalMap).reduce((res, key) => {
+    if (res) return res;
+    if (matchUrl(url, key)) return globalMap[key];
+    return res;
+}, null);
+
+const matchUrl = (url, key) => url.includes(`/${key}/`);
+
+const headers = new Headers({
+    'Content-Type': 'application/javascript'
+});
+
 
 self.addEventListener('activate', event => event.waitUntil(clients.claim()));
+
 
 self.addEventListener('fetch', (event) => {
     let { request: { url } } = event;
 
-    if (url.includes('/library-global/') && url.endsWith('.js')) {
-        console.log(url);
+    console.log('url', url);
+
+    if (
+        url.includes('/library-global/')
+        && url.endsWith('.js')
+    ) {
+        console.log('GLOBAL url', url);
 
         event.respondWith(
-            fetch(url)
+            fetch(event.request)
                 .then(response => response.text())
                 .then(body => {
-                    console.log('JS', url);
-                    return body;
-                })
-                .then(body => new Response(removeSpaces(`
+                    // console.log('body', body);
+
+                    const script = removeSpaces(`
                         const head = document.getElementsByTagName('head')[0];
                         const script = document.createElement('script');
                         script.setAttribute('type', 'text/javascript');
                         script.appendChild(document.createTextNode(${JSON.stringify(body)}));
                         head.appendChild(script);
                         export default window.${getGlobalByUrl(url)};
-                    `), {headers}
-                ))
-        )
+                    `);
+
+                    // console.log(script);
+
+                    return new Response(script, {headers})
+                })
+        );
+        return;
     }
+
+    // event.respondWith(
+    //     fetch(event.request)
+    // );
 });
