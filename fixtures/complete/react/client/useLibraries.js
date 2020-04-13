1aecd59fdeacea883fd97e367a1df46035334eb9
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-13 17:26:10 +0300

    fixtures/complete/react/client > setup: package

--- fixtures/complete/react/client/useLibraries.js
+++ fixtures/complete/react/client/useLibraries.js
@@ -1,69 +1,14 @@
 const {
-    promises: fs,
-} = require('fs');
-const path = require('path');
-
-// const {
-//     libraries,
-//     useLibraries,
-// } = require('@plurid/elementql');
+    libraries,
+    useLibraries,
+} = require('@plurid/elementql');
 
 
 
 const usedLibraries = {
-    'react': {
-        version: 'latest',
-        module: true,
-        development: './node_modules/react/umd/react.development.js',
-        production: './node_modules/react/umd/react.production.min.js',
-    },
-    'react-dom': {
-        version: 'latest',
-        module: true,
-        development: './node_modules/react-dom/umd/react-dom.development.js',
-        production: './node_modules/react-dom/umd/react-dom.production.min.js',
-    },
-};
-
-const useLibraries = async (
-    options,
-) => {
-    const {
-        libraries,
-        buildDirectory,
-    } = options;
-
-    for (const library of Object.values(libraries)) {
-        const libraryPath = path.join(
-            __dirname,
-            library.production,
-        );
-        const appModulePath = path.join(
-            __dirname,
-            buildDirectory,
-            library.production,
-        );
-        const appModuleDirectory = path.dirname(
-            appModulePath,
-        );
-
-        await fs.mkdir(
-            appModuleDirectory,
-            { recursive: true },
-        );
-        await fs.copyFile(
-            libraryPath,
-            appModulePath,
-        );
-    }
+    react: libraries.react,
+    reactDom: libraries.reactDom,
 }
-
-
-
-// const usedLibraries = {
-//     react: libraries.react,
-//     'react-dom': libraries['react-dom'],
-// }
 
 const buildDirectory = 'build';
 
