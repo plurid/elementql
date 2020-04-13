10f1270353d9776e86fa710c0dce5c0239fc9c11
Jan Cavel <caveljan@gmail.com>
2020-04-13 15:59:43 +0300

    fixtures/complete/react/client > dev: files

--- fixtures/complete/react/client/source/service-worker.js
+++ fixtures/complete/react/client/source/service-worker.js
@@ -4,64 +4,97 @@
 // importScripts(elementQLServiceWorker);
 
 
+const generateUtilities = () => {
+    const uriComponent = (decodeURIComponent(self.location.search) || '?{}').slice(1);
 
-const { globals } = JSON.parse((decodeURIComponent(self.location.search) || '?{}').substr(1));
+    const {
+        globals,
+        origins,
+    } = JSON.parse(uriComponent);
+
+    const {
+        elementql,
+        application,
+    } = origins;
+
+    const format = str => str.split(/^ +/m).join('').trim();
+
+    const matchUrl = (url, key) => url.includes(`/${key}/`);
+
+    const getGlobalByUrl = (url) => Object.keys(globals).reduce((res, key) => {
+        if (res) {
+            return res;
+        }
+        if (matchUrl(url, key)) {
+            return globals[key];
+        }
+        return res;
+    }, null);
 
 
-const removeSpaces = str => str.split(/^ +/m).join('').trim();
+    const jsHeaders = new Headers({
+        'Content-Type': 'application/javascript'
+    });
 
-const getGlobalByUrl = (url) => Object.keys(globals).reduce((res, key) => {
-    if (res) return res;
-    if (matchUrl(url, key)) return globals[key];
-    return res;
-}, null);
 
-const matchUrl = (url, key) => url.includes(`/${key}/`);
+    return {
+        uriComponent,
+        globals,
+        origins,
+        elementql,
+        application,
+        format,
+        getGlobalByUrl,
+        matchUrl,
+        jsHeaders,
+    };
+}
 
-const headers = new Headers({
-    'Content-Type': 'application/javascript'
-});
+const utilities = generateUtilities();
+
 
 
 self.addEventListener('activate', event => event.waitUntil(clients.claim()));
 
 
 self.addEventListener('fetch', (event) => {
-    let { request: { url } } = event;
-
-    console.log('url', url);
+    const {
+        request: {
+            url,
+        },
+    } = event;
 
     if (
         url.includes('/node_modules/')
     ) {
-        console.log('GLOBAL url', url);
-        const replace = url.replace('21100/elementql/', '8002/');
-        console.log('replace', replace);
+        const replace = url.replace(utilities.elementql, utilities.application);
 
         event.respondWith(
-            fetch(replace, { mode: 'no-cors' })
-                .then(response => response.text())
-                .then(body => {
-                    // console.log('body', body);
+            fetch(
+                replace,
+                {
+                    mode: 'no-cors',
+                },
+            ).then(
+                response => response.text()
+            ).then(body => {
+                const script = utilities.format(`
+                    const head = document.getElementsByTagName('head')[0];
+                    const script = document.createElement('script');
+                    script.setAttribute('type', 'text/javascript');
+                    script.appendChild(document.createTextNode(${JSON.stringify(body)}));
+                    head.appendChild(script);
+                    export default window.${utilities.getGlobalByUrl(url)};
+                `);
 
-                    const script = removeSpaces(`
-                        const head = document.getElementsByTagName('head')[0];
-                        const script = document.createElement('script');
-                        script.setAttribute('type', 'text/javascript');
-                        script.appendChild(document.createTextNode(${JSON.stringify(body)}));
-                        head.appendChild(script);
-                        export default window.${getGlobalByUrl(url)};
-                    `);
-
-                    // console.log(script);
-
-                    return new Response(script, {headers})
-                })
+                return new Response(
+                    script,
+                    {
+                        headers: utilities.jsHeaders,
+                    },
+                )
+            })
         );
         return;
     }
-
-    // event.respondWith(
-    //     fetch(event.request)
-    // );
 });
