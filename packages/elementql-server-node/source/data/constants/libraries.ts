ec787cbdbcdb1955bd80bf9d65c3dec3f424e4bc
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-13 12:25:07 +0300

    elementql-server-node > dev: libraries

--- packages/elementql-server-node/source/data/constants/libraries.ts
+++ packages/elementql-server-node/source/data/constants/libraries.ts
@@ -8,13 +8,13 @@ export const librariesResolvers: LibrariesResolvers = {
     'react': {
         version: 'latest',
         module: true,
-        development: 'node_modules/react/umd/react.development.js',
-        production: 'node_modules/react/umd/react.production.min.js',
+        development: './node_modules/react/umd/react.development.js',
+        production: './node_modules/react/umd/react.production.min.js',
     },
     'react-dom': {
         version: 'latest',
         module: true,
-        development: 'node_modules/react/umd/react-dom.development.js',
-        production: 'node_modules/react/umd/react-dom.production.min.js',
+        development: './node_modules/react/umd/react-dom.development.js',
+        production: './node_modules/react/umd/react-dom.production.min.js',
     },
 };
