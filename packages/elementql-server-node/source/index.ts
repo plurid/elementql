be51e3b37f0bd6dae2228115221b2a4135c0a937
Jan Cavel <caveljan@gmail.com>
2020-04-13 17:22:39 +0300

    elementql-server-node > setup: package

--- packages/elementql-server-node/source/index.ts
+++ packages/elementql-server-node/source/index.ts
@@ -1,15 +1,8 @@
 import ElementQLServer from './objects/ElementQLServer';
 
-import {
-    librariesResolvers as libraries,
-} from './data/constants/libraries';
-
 
 
 export * from './data/interfaces';
 
-export {
-    libraries,
-}
 
 export default ElementQLServer;
