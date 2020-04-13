722d629ea1d627697887396d70d26d43831cc0f2
Jan Cavel <caveljan@gmail.com>
2020-04-13 16:20:22 +0300

    fixtures/complete/react/client > dev: metabootloader

---
+++ fixtures/complete/react/client/source/index-config.js
@@ -0,0 +1,37 @@
+const configuration = {
+    globals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+    },
+    origins: {
+        elementql: 'http://localhost:21100/elementql',
+        application: 'http://localhost:8002',
+    },
+};
+
+
+const bootloader = async (
+    configuration,
+) => {
+    try {
+        await navigator.serviceWorker.register('service-worker.js?' + JSON.stringify(configuration));
+        await navigator.serviceWorker.ready;
+
+        const launch = async () => {
+            await import('./app/index.jsx');
+        };
+
+        // this launches the React app if the SW has been installed before or immediately after registration
+        // https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle#clientsclaim
+        if (navigator.serviceWorker.controller) {
+            await launch();
+        } else {
+            navigator.serviceWorker.addEventListener('controllerchange', launch);
+        }
+    } catch (error) {
+        console.error('Service worker registration failed', error.stack);
+    }
+}
+
+
+bootloader(configuration);
