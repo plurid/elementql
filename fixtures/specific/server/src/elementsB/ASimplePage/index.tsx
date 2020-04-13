45b5d39237a5e9277500e64cb2544c784c428d03
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-13 11:31:09 +0300

    fixtures/specific > dev: client-react, server

---
+++ fixtures/specific/server/src/elementsB/ASimplePage/index.tsx
@@ -0,0 +1,15 @@
+// @ts-nocheck
+import React from 'react';
+
+
+
+const ASimplePage: React.FC<any> = () => {
+    return (
+        <div>
+            A Simple Page
+        </div>
+    );
+}
+
+
+export default ASimplePage;
