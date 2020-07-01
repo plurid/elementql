c2047d4300920b0705a8e445d9ab0dec1ca5040a
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-07-01 11:22:17 +0300

    elementql-client > dev: ElementQLClient

--- packages/elementql-client/source/objects/ElementQLClient/index.ts
+++ packages/elementql-client/source/objects/ElementQLClient/index.ts
@@ -33,8 +33,6 @@ class ElementQLClient {
     private async getWithElementQL(
         request: any,
     ) {
-        console.log('request', request);
-
         const elementFiles = await fetch(this.url,
             {
                 method: 'POST',
@@ -43,28 +41,20 @@ class ElementQLClient {
                     'Content-Type': 'application/elementql'
                 }
             }).then(res => {
-                console.log(res);
                 return res.json();
             })
             .catch(error => console.error('Error:', error));
 
-        console.log('elementFiles', elementFiles);
-
-        for (let element of elementFiles) {
+        for (const element of elementFiles) {
             if (element.js) {
-                console.log('injected script');
                 await injectScript(element.js);
             }
             if (element.css) {
-                console.log('injected style');
                 await injectStyle(element.css);
             }
         }
 
-        console.log(window.elementql)
-
         if (window.elementql) {
-            console.log('Return Element based on Id/Name');
             if (window.elementql.element) {
                 return window.elementql.element;
             }
